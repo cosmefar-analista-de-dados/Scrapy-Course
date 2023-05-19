2688bc74da2e776868f6b0d48d87e1c0df9cb6c6
cosmefar-analista-de-dados <analistacomercial1@cosmefar.com.br>
2023-05-19 15:27:21 -0300

    part 5 - completed

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -19,7 +19,35 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 book_url = 'https://books.toscrape.com/catalogue/' + relative_url
 
-            yield response.folllow(book_url, callback = self.parse_book_page)
+            yield response.follow(book_url, callback = self.parse_book_page)
+
+        next_page = response.css('li.next a').attrib['href']
+
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+
+            yield response.follow(next_page_url, callback = self.parse)
 
     def parse_book_page(self, response):
-        pass
+
+        table_rows = response.css('table tr')
+        
+        yield {
+
+            'url' : response.url,
+            'title' : response.css('.product_main h1::text').get(),
+            'product-type' : table_rows[1].css('td ::text').get(),
+            'price_excl_tax' : table_rows[2].css('td ::text').get(),
+            'price_incl_tax' : table_rows[3].css('td ::text').get(),
+            'tax' : table_rows[4].css('td ::text').get(),
+            'availability' : table_rows[5].css('td ::text').get(),
+            'num_reviews' : table_rows[6].css('td ::text').get(),
+            'stars' : response.xpath('//*[@id="content_inner"]/article/div[1]/div[2]/p[3]').attrib['class'],
+            'category' : response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()').get(),
+            'description' : response.xpath('//*[@id="content_inner"]/article/p/text()').get(),
+            'price' : response.css('.product_main .price_color::text').get()
+
+        }
