acc63c984ff0ec99100973dbd5909f837a7c696f
cosmefar-analista-de-dados <analistacomercial1@cosmefar.com.br>
2023-05-19 16:55:03 -0300

    part 7 - working

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -6,8 +6,56 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import re
 
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        ## !Description ==> item.strip()
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+
+        ##Category & Product ==> item.lowercase()
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        ##Price ==> float(item)
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+
+        ##Availability ==> remove extra text from strings
+        value = adapter.get('availability')
+        value = re.findall('\d+', str(value))[0]
+        adapter['availability'] = float(value)
+
+        ##Reviews ==> int(item)
+        value = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(value)
+
+        ##Stars ==> converting to readable values
+        value = adapter.get('stars')
+        value = value.split(' ')
+        value = value[1].lower()
+        if value == "one":
+            adapter['stars'] = 1
+        elif value == 'two':
+            adapter['stars'] = 2
+        elif value == "three":
+            adapter['stars'] = 3
+        elif value == "four":
+            adapter['stars'] = 4
+        elif value == "five":
+            adapter['stars'] = 5
+
         return item
--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,3 +1,4 @@
+from bookscraper.items import BookItem
 import scrapy
 
 
@@ -34,20 +35,19 @@ class BookspiderSpider(scrapy.Spider):
     def parse_book_page(self, response):
 
         table_rows = response.css('table tr')
-        
-        yield {
+        book_item = BookItem()
 
-            'url' : response.url,
-            'title' : response.css('.product_main h1::text').get(),
-            'product-type' : table_rows[1].css('td ::text').get(),
-            'price_excl_tax' : table_rows[2].css('td ::text').get(),
-            'price_incl_tax' : table_rows[3].css('td ::text').get(),
-            'tax' : table_rows[4].css('td ::text').get(),
-            'availability' : table_rows[5].css('td ::text').get(),
-            'num_reviews' : table_rows[6].css('td ::text').get(),
-            'stars' : response.xpath('//*[@id="content_inner"]/article/div[1]/div[2]/p[3]').attrib['class'],
-            'category' : response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()').get(),
-            'description' : response.xpath('//*[@id="content_inner"]/article/p/text()').get(),
-            'price' : response.css('.product_main .price_color::text').get()
+        book_item['url'] = response.url
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['product_type'] = table_rows[1].css('td ::text').get()
+        book_item['price_excl_tax'] = table_rows[2].css('td ::text').get()
+        book_item['price_incl_tax'] = table_rows[3].css('td ::text').get()
+        book_item['tax'] = table_rows[4].css('td ::text').get()
+        book_item['availability'] = table_rows[5].css('td ::text').get()
+        book_item['num_reviews'] = table_rows[6].css('td ::text').get()
+        book_item['stars'] = response.xpath('//*[@id="content_inner"]/article/div[1]/div[2]/p[3]').attrib['class']
+        book_item['category'] = response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()').get()
+        book_item['description'] = response.xpath('//*[@id="content_inner"]/article/p/text()').get()
+        book_item['price'] = response.css('.product_main .price_color::text').get()
 
-        }
+        yield book_item
